61a8fe279a97814ed8215c723430fb3b717cf713
drewcodemusic <100270982+drewcodemusic@users.noreply.github.com>
2022-06-14 08:58:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 # my first push to github
 # my second push
 
-print('make a branch edit')
+# print('make a branch edit')
 print(datetime.now())
 
 print('some changes done on remote repo')
